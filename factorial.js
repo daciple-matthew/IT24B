4446dedafc1835b7840c193d8bdea7db87a293ab
daciple-matthew <daciplerosemateo@gmail.com>
2025-09-09 08:50:50 +0800

    Added mainMenu() function and initial instance

--- factorial.js
+++ factorial.js
@@ -1,44 +1,59 @@
-const readline=require("readline");
+//Console Input
+const readline = require("readline");
 
-const r1 = readline.createInterface({
+const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
-console.log("Factorial Application");
-console.log("------------------------------");
-console.log("1. Say Hello");
-console.log("2. Factorial");
-console.log("3. Exit Application");
-console.log("------------------------------");
+function mainMenu() {
 
-r1.question("enter your choice (1-3): ", choice =>{
-    console.log(choice);
+    console.log("---------------------------");
+    console.log("Factorial Application");
+    console.log("1. Say Hello");
+    console.log("2. Factorial");
+    console.log("3. Exit Application");
+    console.log("---------------------------");
 
-if (choice === "1"){
-    console.log("Hello");
-} else if (choice === "2"){
-    console.log("Factorial");
-} else if (choice === "3"){
-    console.log("Exiting Application");
-} else {
-    console.log("Invalid Choice. Please Try Again");
+    rl.question("Enter your choice (1-3): ", choice => {
+        console.log(choice);
+
+        if (choice === "1") {
+            sayHello();
+
+        } else if (choice === "2") {
+            computeFactorial();
+
+            rl.question("Please enter a number for factorial: ", numStr => {
+                let num = parseInt(numStr);
+
+                if (isNaN(num) || num < 0) {
+                    console.log("Please enter a non-negative integer");
+                } else {
+                    let fact = 1;
+
+                    for (let i = 1; i <= num; i++) {
+                        fact *= i;
+                    }
+
+                    console.log("The factorial of " + num + " is " + fact);
+                    mainMenu();
+
+                }
+            })
+
+
+
+        } else if (choice === "3") {
+            exitProgram();
+        } else {
+            console.log("Invalid Choice. Please Try Again");
+        }
+
+    })
 }
 
-})
 
-    r1.question("Please enter a number for factorial: ", numStr =>{
-        let num = parseInt(numStr);
 
-        if (isNaN(num) || num < 0){
-            console.log("Please enter a non-negative integer");
-        } else {
-            let fact = 1;
 
-            for (let i=1; i <= num; i++){
-                fact *=1;
-        }
-        
-        console.log("The Factorial of " + num + " is " + fact);
-    }
-    })
+mainMenu();
